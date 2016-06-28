f00dd21460a612c048d006537f6c2db0933e883a
Simo Larvi <simo.larvi@cs.tamk.fi>
2016-06-28 13:22:42 +0300

    PlayerMovement improved

--- Notfootball/Assets/Scripts/PlayerMovement.cs
+++ Notfootball/Assets/Scripts/PlayerMovement.cs
@@ -4,75 +4,25 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	private string playerTag;
 	private Rigidbody body;
-
 	public float movSpeed;
 
 	public PlayerInput playerInput;
 
 	// Use this for initialization
 	void Start () {
-		playerTag = gameObject.tag;
 		body = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Move (gameObject.tag, body);
+	}
 
-		if (playerTag.Equals ("Player1")) {
-			Vector3 force = Vector3.zero;
-			/*
-			if (Input.GetKey (KeyCode.A)) {
-				force += Vector3.left;
-				//transform.Translate (Vector3.left * movSpeed * Time.deltaTime);
-			}
-
-			if (Input.GetKey (KeyCode.W)) {
-				force += Vector3.forward;
-				//transform.Translate (Vector3.forward * movSpeed * Time.deltaTime);
-			}
-
-			if (Input.GetKey (KeyCode.D)) {
-				force += Vector3.right;
-				//transform.Translate (Vector3.right * movSpeed * Time.deltaTime);
-			}
-
-			if (Input.GetKey (KeyCode.S)) {
-				force += Vector3.back;
-				//transform.Translate (Vector3.back * movSpeed * Time.deltaTime);
-			}
-			*/
-
-			force = new Vector3 (playerInput.GetXInput ("Player1"), 0, playerInput.GetYInput ("Player1")); 
-			force = force.normalized * movSpeed * Time.deltaTime;
-			body.velocity = force;
-		}
-
-		if (playerTag.Equals ("Player2")) {
-
-			Vector3 force = Vector3.zero;
-
-			if (Input.GetKey (KeyCode.LeftArrow)) {
-				force += Vector3.left;
-				//transform.Translate (Vector3.left * movSpeed * Time.deltaTime);
-			}
-
-			if (Input.GetKey (KeyCode.UpArrow)) {
-				force += Vector3.forward;
-				//transform.Translate (Vector3.forward * movSpeed * Time.deltaTime);
-			}
-
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				force += Vector3.right;
-				//transform.Translate (Vector3.right * movSpeed * Time.deltaTime);
-			}
-
-			if (Input.GetKey (KeyCode.DownArrow)) {
-				force += Vector3.back;
-				//transform.Translate (Vector3.back * movSpeed * Time.deltaTime);
-			}
-
-			force = force.normalized * movSpeed * Time.deltaTime;
-			body.velocity = force;
-		}
+	void Move(string player, Rigidbody b) {
+		Vector3 force = Vector3.zero;
+		force = new Vector3 (playerInput.GetXInput (player), 0, playerInput.GetYInput (player)); 
+		force = force.normalized * movSpeed * Time.deltaTime;
+		b.velocity = force;
+		print (player + " " + force);
 	}
 }
