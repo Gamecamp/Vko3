71dbc4443ceffb768a3754d289f56019cf6abfd6
iikkap <iikka.pohjola@cs.tamk.fi>
2016-06-28 15:37:38 +0300

    Assets and dasherinos
    Added assets and the ability to dash (only works for player 2 for now)
    and fixed "gravity" so that players fall.

--- Notfootball/Assets/Scripts/PlayerInput.cs
+++ Notfootball/Assets/Scripts/PlayerInput.cs
@@ -7,6 +7,7 @@ public class PlayerInput : MonoBehaviour {
 
 	private float player2InputXAxis;
 	private float player2InputYAxis;
+	private KeyCode player2Dash;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,8 @@ public class PlayerInput : MonoBehaviour {
 		for (int i = 0; i < names.Length; i++) {
 			print(names[i]);
 		}
+
+		player2Dash = KeyCode.Space;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +28,8 @@ public class PlayerInput : MonoBehaviour {
 
 		player2InputXAxis = Input.GetAxis ("Horizontal2");
 		player2InputYAxis = Input.GetAxis ("Vertical2");
+
+
 
 		//print ("horizontal " + Input.GetAxis ("Horizontal"));
 		//print ("vertical2 " + Input.GetAxis ("Vertical2"));
@@ -54,5 +59,22 @@ public class PlayerInput : MonoBehaviour {
 			input = player2InputYAxis;
 		}
 		return input; 
+	}
+
+	public bool GetDash(string player) {
+
+		bool dash = false;
+
+		if (player.Equals ("Player1")) {
+
+		}
+
+		if (player.Equals ("Player2")) {
+			if (Input.GetKey (player2Dash)) {
+				dash = true;
+			}
+		}
+
+		return dash;
 	}
 }
--- Notfootball/Assets/Scripts/PlayerMovement.cs
+++ Notfootball/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,15 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	private string playerTag;
 	private Rigidbody body;
+	private bool dashGoing;
+	private float player2DashDuration;
+	private Vector3 player2DashVector;
+
 
 	public float movSpeed;
+
+	public float dashDuration;
+	public float dashMultiplier;
 
 	public PlayerInput playerInput;
 
@@ -13,6 +20,8 @@ public class PlayerMovement : MonoBehaviour {
 	void Start () {
 		playerTag = gameObject.tag;
 		body = GetComponent<Rigidbody> ();
+	
+		player2DashDuration = dashDuration;
 	}
 	
 	// Update is called once per frame
@@ -44,6 +53,8 @@ public class PlayerMovement : MonoBehaviour {
 
 			force = new Vector3 (playerInput.GetXInput ("Player1"), 0, playerInput.GetYInput ("Player1")); 
 			force = force.normalized * movSpeed * Time.deltaTime;
+
+			force.y = body.velocity.y;
 			body.velocity = force;
 		}
 
@@ -51,27 +62,54 @@ public class PlayerMovement : MonoBehaviour {
 
 			Vector3 force = Vector3.zero;
 
-			if (Input.GetKey (KeyCode.LeftArrow)) {
-				force += Vector3.left;
-				//transform.Translate (Vector3.left * movSpeed * Time.deltaTime);
+			if (!dashGoing) {
+
+				if (Input.GetKey (KeyCode.LeftArrow)) {
+					force += Vector3.left;
+					//transform.Translate (Vector3.left * movSpeed * Time.deltaTime);
+				}
+
+				if (Input.GetKey (KeyCode.UpArrow)) {
+					force += Vector3.forward;
+					//transform.Translate (Vector3.forward * movSpeed * Time.deltaTime);
+				}
+
+				if (Input.GetKey (KeyCode.RightArrow)) {
+					force += Vector3.right;
+					//transform.Translate (Vector3.right * movSpeed * Time.deltaTime);
+				}
+
+				if (Input.GetKey (KeyCode.DownArrow)) {
+					force += Vector3.back;
+					//transform.Translate (Vector3.back * movSpeed * Time.deltaTime);
+				}
+
+				force = force.normalized * movSpeed * Time.deltaTime;
+
+
+			} else {
+				force = player2DashVector;
 			}
 
-			if (Input.GetKey (KeyCode.UpArrow)) {
-				force += Vector3.forward;
-				//transform.Translate (Vector3.forward * movSpeed * Time.deltaTime);
+			if (playerInput.GetDash ("Player2") == true && !dashGoing) {
+				player2DashVector = force * dashMultiplier;
+				print ("DASH");
+				dashGoing = true;
 			}
 
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				force += Vector3.right;
-				//transform.Translate (Vector3.right * movSpeed * Time.deltaTime);
+			if (dashGoing) {
+				player2DashDuration = player2DashDuration - 0.1f;
+				print ("Dash: " + player2DashDuration);
+				force = force * dashMultiplier;
+
+				if (player2DashDuration <= 0) {
+					dashGoing = false;
+					player2DashDuration = dashDuration;
+				}
 			}
 
-			if (Input.GetKey (KeyCode.DownArrow)) {
-				force += Vector3.back;
-				//transform.Translate (Vector3.back * movSpeed * Time.deltaTime);
-			}
+			force.y = body.velocity.y;
 
-			force = force.normalized * movSpeed * Time.deltaTime;
 			body.velocity = force;
 		}
 	}
