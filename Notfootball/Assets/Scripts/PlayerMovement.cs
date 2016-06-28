3c54afe980d1b85202141df055dcfd3a680882a8
Simo Larvi <simo.larvi@cs.tamk.fi>
2016-06-28 10:35:56 +0300

    Preliminary player movement and playfield done

---
+++ Notfootball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private string playerTag;
+	private Rigidbody body;
+	public float movSpeed;
+
+	// Use this for initialization
+	void Start () {
+		playerTag = gameObject.tag;
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (playerTag.Equals ("Player1")) {
+
+		}
+		*/
+		Vector3 force = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.A)) {
+			force += Vector3.left * movSpeed * Time.deltaTime;
+			//transform.Translate (Vector3.left * movSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			force += Vector3.forward * movSpeed * Time.deltaTime;
+			//transform.Translate (Vector3.forward * movSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			force += Vector3.right * movSpeed * Time.deltaTime;
+			//transform.Translate (Vector3.right * movSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			force += Vector3.back * movSpeed * Time.deltaTime;
+			//transform.Translate (Vector3.back * movSpeed * Time.deltaTime);
+		}
+
+		body.velocity = force;
+		print (force);
+	}
+}
