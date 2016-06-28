4607d1dae74663f3799cad73ceb9423a76aa38f9
banbanxaixai <anton.peltoniemi@gmail.com>
2016-06-28 11:19:52 +0300

    Add Prefabs folder with a simple goal counter
    Add Prefabs folder with a simple goal counter and its Scoreboard script.
    Call Canvas's BlueTeamScores or RedTeamScores when goal is scored!

---
+++ Notfootball/Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour {
+
+	Text blueScore, redScore;
+	int blueGoals, redGoals;
+
+	// Use this for initialization
+	void Start () {
+		blueScore = GameObject.Find ("Blue Team Score").GetComponent<Text>();
+		redScore = GameObject.Find ("Red Team Score").GetComponent<Text>();
+		blueGoals = 0;
+		redGoals = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BlueTeamScores() {
+		blueGoals++;
+		ChangeScore ();
+	}
+
+	public void RedTeamScores() {
+		redGoals++;
+		ChangeScore ();
+	}
+
+	void ChangeScore() {
+		blueScore.text = "" + blueGoals;
+		redScore.text = "" + redGoals;
+	}
+}
