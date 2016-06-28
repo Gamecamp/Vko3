8769edd819088ddf476a165c8f9d21928e2d73a2
iikkap <iikka.pohjola@cs.tamk.fi>
2016-06-28 12:52:56 +0300

    Goals
    Added functionality and a prefab thing for goals

---
+++ Notfootball/Assets/Scripts/GoalChecker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalChecker : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Ball") {
+			if (gameObject.tag == "GoalP1") {
+				print ("blueTeamScore");
+
+			}
+
+			if (gameObject.tag == "GoalP2") {
+				print ("redTeamScore");
+			}
+		}
+
+		print ("blaa");
+	}
+}
